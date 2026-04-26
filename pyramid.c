4dac1405022e0092eed3fe655b970cafe80ad53b
Emiliano Sigales <emilianosigalesgomez@gmail.com>
2026-04-26 13:42:27 +0200

    Add pyramid printing program in C

---
+++ pyramid.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int bricks);
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+        print_row(i + 1);
+    }
+}
+
+void print_row(int bricks)
+{
+    for (int  i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
